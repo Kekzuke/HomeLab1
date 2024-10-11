d86cbb1995006c061a171a877584f195bbc2ae23
Artem Golub <artem.golub2019@gmail.com>
2024-10-11 22:06:28 +0300

    Add 'Welcome' message in different languages

--- hello.pas
+++ hello.pas
@@ -1,2 +1,11 @@
 ﻿##
-Print('Hello world!');
+Println('Поддерживаемые языки: Русский(ru), Белорусский(be), Английский(en), Немецкий(de)');
+var lang := ReadString('Выберите язык(пример ввода: en):');
+case lang of
+  'en': Println('Hello!');
+  'de': Println('Hallo!');
+  'be': Println('Прывітанне!');
+  'ru'qw: Println('Привет!')
+else
+  Println('Неправильный код языка!');
+end;
